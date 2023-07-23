913f459c1b6d10819fa2c8ce2e661a0c710269b0
fukuchiakiho <c0b20135d2@edu.teu.ac.jp>
2023-07-23 19:53:52 +0900

    推論

--- code/BB-RNN/MAIN/pred_bb.py
+++ code/BB-RNN/MAIN/pred_bb.py
@@ -3,23 +3,31 @@ import sys
 sys.path.append('..')
 import os
 from common.optimizer import SGD
-from common.trainer import RnnlmTrainer
+from common.functions import softmax
+#from common.trainer import RnnlmTrainer
 import pandas as pd
-from simple_rnnbb import SimpleRnnbb
+from simple_rnnbb import Pred_simpleRnnbb
 import numpy as np
+import pickle
 
-name1 = "20220407_0831_SabrVec_learn"
+name1 = "20220407_0831_SabrVec_test"
+name3 = "20220407_0831_SabrVec_learn"
 name2 = "test"
-path1 = os.path.join('/home/fkch/mlb/data/vecdata/' + name1 + '.csv')
+path1 = os.path.join('/home/fkch/mlb/data/vecdata/' + name3 + '.csv')
 path2 = os.path.join('/home/fkch/mlb/' + name2 + '.csv')
+#path3 = os.path.join('/home/fkch/mlb/data/vecdata/' + name3 + '.csv')
 reader1 = pd.read_csv(path1, sep=',')
 reader2 = pd.read_csv(path2, sep=',')
+#reader3 = 
 df = pd.merge(reader1, reader2, on='play', how= 'left')
 df_x = df.fillna({'ID': 15})
 m = 0
 z = 1
 max_mask = []
 max_counter = []
+time_idx = 10
+with open('/home/fkch/mlb/code/BB-RNN/MAIN/Rnnlm.pkl', 'rb') as f:
+    params = pickle.load(f)
 
 def team(x):
     global m, z
@@ -51,6 +59,22 @@ for jj in range(1, max_num+1):
     for i in range(1,max_i+1):
         a = nm[nm['Inn.'] == i]
         max_counter.append(len(a))
+
+def get_batch(x, t, batch_size, time_size, inn_size):
+        global time_idx
+        batch_x = np.empty((batch_size, time_size, inn_size), dtype='i')
+        batch_t = np.empty((batch_size, time_size), dtype='i')
+
+        data_size = len(x)
+        jump = data_size // batch_size
+        offsets = [i * jump for i in range(batch_size)]  # バッチの各サンプルの読み込み開始位置
+
+        for time in range(time_size):
+            for i, offset in enumerate(offsets):
+                batch_x[i, time] = x[(offset + time_idx) % data_size]
+                batch_t[i, time] = t[(offset + time_idx) % data_size]
+            time_idx += 1
+        return batch_x, batch_t
 
 #変数
 max_sequence_len = df_x['Inn.'].max()
@@ -102,34 +126,19 @@ for j in range(1, max_num+1):
             #tx = []
             tx = row['ID']
             tm.append(tx)
-        #for b in range(re_pa):
-            #tm.append(11.0)
-            #m.append([])
-            #mlpa = len(m)
-            #for bb in range(len(m[0])):
-                #m[mlpa-1].append(0.0)
-    #for b in range(re_inn):
-        #for bb in range(max_PA):
-            #tm.append(11.0)
-            #m.append([])
-            #mlin = len(m)
-            #for bbb in range(len(m[0])):
-                #m[mlin-1].append(0.0)
-
-#出力データ
-'''
-for j in range(1, max_num+1):
-    nm = df_x[df_x['number'] == j]
-    max_i = nm['Inn.'].max()
-    re_inn = max_sequence_len - max_i
-    for i in range(1,max_i+1):
-        a = nm[nm['Inn.'] == i]
-        for index, row in a.iterrows():
-            tx = row['ID']
-            tm.append(tx)
-        for x in range(re_inn):
+        for b in range(re_pa):
             tm.append(11.0)
-            #tx = []'''
+            m.append([])
+            mlpa = len(m)
+            for bb in range(len(m[0])):
+                m[mlpa-1].append(0.0)
+    for b in range(re_inn):
+        for bb in range(max_PA):
+            tm.append(11.0)
+            m.append([])
+            mlin = len(m)
+            for bbb in range(len(m[0])):
+                m[mlin-1].append(0.0)
 
 #ndarray形式にキャスト
 m = np.array(m)
@@ -146,23 +155,62 @@ hidden_size = 10  # RNNの隠れ状態ベクトルの要素数
 inn_size = 13
 time_size = 17  # RNNを展開するサイズ
 lr = 0.1
-max_epoch = 100
+max_epoch = 20
 index_size = len(max_il)
 #print(index_size)
+
 
 # 学習データの読み込み
 
 xs = m  # 入力
-#print(xs.shape)
+print(xs.shape)
 ts = tm # 出力（教師ラベル）play
-#print(ts.shape)
+print(ts.shape)
+#for i in range()
+batch_x, batch_t = get_batch(xs, ts, batch_size, time_size, inn_size)
 
 # モデルの生成
-model = SimpleRnnbb(output_size, input_size, hidden_size)
+model = Pred_simpleRnnbb(output_size, input_size, hidden_size, params)
 optimizer = SGD(lr)
-trainer = RnnlmTrainer(model, optimizer)
+#trainer = RnnlmTrainer(model, optimizer)
 
-trainer.fit(xs, ts, max_epoch, batch_size, inn_size=inn_size, time_size=time_size)
-trainer.plot()
+#trainer.fit(xs, ts, max_epoch, batch_size, inn_size=inn_size, time_size=time_size)
+#trainer.plot()
+'''
+def generate(xs, skip_ids=None, sample_size=100):
+    #word_ids = [start_id]
 
-model.save_params()
+    x = xs[:sample_size]
+    #while len(word_ids) < sample_size:
+    for i in x:
+    #i = np.array(i).reshape(1, 1)
+        score = model.predict(i)
+    #print(score)
+        p = softmax(score.flatten())
+    #print(p)
+        sampled = np.random.choice(len(p), size=1, p=p)
+        #print(sampled)
+        #if (skip_ids is None) or (sampled not in skip_ids):
+            #x = sampled
+            #word_ids.append(int(x))
+
+    return sampled'''
+nend = []
+test_kekka = np.empty(0)
+s = model.forward(batch_x, batch_t)
+for i in s:
+    #print(ii)
+    sampled = np.random.choice(len(i), size=1, p=i)
+    #i_max = np.argmax(i)
+
+    #np.append(test_kekka, sampled)
+    #nend.append(i_max)
+    nend.append(sampled)
+ssss = np.array(nend)
+ssss = ssss.flatten()
+#c = (ssss == ts)
+print("予測")
+print(ssss)
+batch_t = batch_t.flatten()
+print("正解")
+print(batch_t)
--- code/BB-RNN/MAIN/simple_rnnbb.py
+++ code/BB-RNN/MAIN/simple_rnnbb.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('..')
 import numpy as np
+#from common.time_layers_pred import *
 from common.time_layers import *
 import pickle
 
@@ -29,7 +30,7 @@ class SimpleRnnbb:
             TimeAffine(affine_W, affine_b)
         ]
         self.loss_layer = TimeSoftmaxWithLoss()
-        self.rnn_layer = self.layers[1]
+        self.rnn_layer = self.layers[0]
 
         # すべての重みと勾配をリストにまとめる
         self.params, self.grads = [], []
@@ -55,3 +56,66 @@ class SimpleRnnbb:
     def save_params(self, file_name='Rnnlm.pkl'):
         with open(file_name, 'wb') as f:
             pickle.dump(self.params, f)
+
+class Pred_simpleRnnbb:
+    def __init__(self, output_size, input_size, hidden_size, params):
+        V, D, H = output_size, input_size, hidden_size
+        #rn = np.random.randn
+        # 重みの初期化
+        ''''''
+        #rnn_Wx = (rn(D, H) / np.sqrt(D)).astype('f')
+        #rnn_Wh = (rn(H, H) / np.sqrt(H)).astype('f')
+        #rnn_b = np.zeros(H).astype('f')
+        #affine_W = (rn(H, V) / np.sqrt(H)).astype('f')
+        #affine_b = np.zeros(V).astype('f')
+
+        rnn_Wx = params[0]
+        rnn_Wh = params[1]
+        rnn_b = params[2]
+        affine_W = params[3]
+        affine_b = params[4]
+        '''
+        rnn_Wx = rnn_Wx.tolist()
+        rnn_Wh = rnn_Wh.tolist()
+        rnn_b = rnn_b.tolist()
+        affine_W = affine_W.tolist()
+        affine_b = affine_b.tolist()
+        '''
+        # レイヤの生成
+        self.layers = [
+            TimeRNN(rnn_Wx, rnn_Wh, rnn_b, stateful=True),
+            TimeAffine(affine_W, affine_b)
+        ]
+        self.loss_layer = TimeSoftmaxWithLoss()
+        self.rnn_layer = self.layers[0]
+
+        # すべての重みと勾配をリストにまとめる
+        self.params, self.grads = [], []
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, xs, ts):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        ys = self.loss_layer.forward(xs, ts)
+        return ys
+    
+    def predict(self, xs):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        return xs
+
+    '''
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
+
+    def reset_state(self):
+        self.rnn_layer.reset_state()
+
+    def save_params(self, file_name='Rnnlm.pkl'):
+        with open(file_name, 'wb') as f:
+            pickle.dump(self.params, f)'''
